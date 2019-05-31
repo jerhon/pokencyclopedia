3d905acf04ff06c8c5f703312bf0a9c3cbf7b45b
Jeremy Honl <jerhon@users.noreply.github.com>
2019-05-30 23:37:33 -0500

    Change to Pokencyclopedia

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Pokedex
+# Pokencyclopedia
 
 This is a sample application for me to play around with Angular. It utilizes [PokeAPI](https://pokeapi.co/) to get information and display it.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to honlsoft-pokedex!');
+    expect(page.getTitleText()).toEqual('Welcome to pokencyclopedia!');
   });
 
   afterEach(async () => {
